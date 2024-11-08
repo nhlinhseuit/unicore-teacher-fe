e123d690c7096976f96864e38469179df7fcc2ef
Nguyễn Hoàng Linh <dev.hoanglinh@gmail.com>
2024-11-08 16:54:33 +0700

    time submit info

--- app/(root)/courses/[courseId]/exercises/page.tsx
+++ app/(root)/courses/[courseId]/exercises/page.tsx
@@ -4,6 +4,8 @@ import ExercisePostItem from "@/components/courses/ExercisePostItem";
 import BackToPrev from "@/components/shared/BackToPrev";
 import IconButton from "@/components/shared/Button/IconButton";
 import GradingGroupTable from "@/components/shared/Table/TableGrading/GradingGroupTable";
+import { Dropdown } from "flowbite-react";
+import Image from "next/image";
 import React, { useState } from "react";
 
 const Exercises = () => {
@@ -161,24 +163,89 @@ const Exercises = () => {
       />
 
       <div className="flex justify-between mb-6">
-        <p className="paragraph-semibold ml-4">Bài tập ngày 29/08/2024</p>
+        <div className="ml-4 flex gap-4 items-center">
+          <p className="paragraph-semibold">Bài tập ngày 29/08/2024</p>
+          <Dropdown
+            className="z-30 rounded-lg"
+            label=""
+            renderTrigger={() => (
+              <Image
+                src={"/assets/icons/info.svg"}
+                width={18}
+                height={18}
+                alt={"edit"}
+                className={`object-contain cursor-pointer`}
+              />
+            )}
+          >
+            <Dropdown.Header>
+              <span className="block text-sm font-medium text-center truncate">
+                Thông tin
+              </span>
+            </Dropdown.Header>
+            <div className="scroll-container scroll-container-dropdown-content">
+              <ul>
+                <li role="menuitem">
+                  <button
+                    type="button"
+                    className="flex items-center justify-start w-full px-4 py-2 text-sm text-gray-700 cursor-default dark:text-gray-200 "
+                  >
+                    <span className="font-semibold">Thời hạn nộp bài:</span>
+                    <span> 12h SA 8/11/2024 - 11h30 SA 15/11/2024</span>
+                  </button>
+                </li>
+                <li role="menuitem">
+                  <button
+                    type="button"
+                    className="flex items-center justify-start w-full px-4 py-2 text-sm text-gray-700 cursor-default dark:text-gray-200 "
+                  >
+                    <span className="font-semibold">Thời hạn nộp trễ:</span>
+                    <span>12h SA 8/11/2024 - 11h30 SA 15/11/2024</span>
+                  </button>
+                </li>
+                <li role="menuitem">
+                  <button
+                    type="button"
+                    className="flex items-center justify-start w-full px-4 py-2 text-sm text-gray-700 cursor-default dark:text-gray-200 "
+                  >
+                    <span className="font-semibold">
+                      Thời hạn đóng bài nộp:
+                    </span>
+                    <span>12h SA 16/11/2024</span>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </Dropdown>
+          {/* <p className="mt-2 text-sm font-medium">
+            
+          </p>
+          <p className="mt-2 text-sm">
+           
+          </p>
+          <p className="mt-2 text-sm">
+           
+          </p> */}
+        </div>
 
-        {isEditTable ? (
-          <IconButton
-            text="Lưu"
-            onClick={() => {
-              setIsEditTable(false);
-            }}
-          />
-        ) : (
-          <IconButton
-            text="Chấm điểm"
-            green
-            onClick={() => {
-              setIsEditTable(true);
-            }}
-          />
-        )}
+        <div>
+          {isEditTable ? (
+            <IconButton
+              text="Lưu"
+              onClick={() => {
+                setIsEditTable(false);
+              }}
+            />
+          ) : (
+            <IconButton
+              text="Chấm điểm"
+              green
+              onClick={() => {
+                setIsEditTable(true);
+              }}
+            />
+          )}
+        </div>
       </div>
 
       <GradingGroupTable
