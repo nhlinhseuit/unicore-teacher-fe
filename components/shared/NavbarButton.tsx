3177099c2aee7e123c9a233dda807815d88c74f3
Nguyễn Hoàng Linh <dev.hoanglinh@gmail.com>
2024-12-01 14:05:21 +0700

    fix navbar button color

--- components/shared/NavbarButton.tsx
+++ components/shared/NavbarButton.tsx
@@ -10,9 +10,9 @@ const NavbarButton = (params: Props) => {
     <button
       type="button"
       className={`${
-        !params.isActive ? "hover:bg-[#ECF2FFFF] hover:text-[#5D87FFFF]" : ""
+        !params.isActive ? "hover:bg-[#f6f9fc] hover:text-[#5D87FFFF]" : ""
       } rounded-lg flex items-center justify-center px-4 py-2 text-sm font-medium first:ml-0 disabled:cursor-not-allowed disabled:text-gray-400 disabled:dark:text-gray-500  ${
-        params.isActive ? "bg-primary-500 text-white" : ""
+        params.isActive ? "bg-[#ECF2FFFF] text-[#5D87FFFF]" : ""
       }`}
       role="tab"
     >
