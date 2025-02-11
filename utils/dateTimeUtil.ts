22ec7e7da1921fd7ecc63bca12ea5b0ce0f102af
nhlinhizota <93470801+nhlinhseuit@users.noreply.github.com>
2025-02-11 10:34:16 +0700

    fix utils

--- utils/dateTimeUtil.ts
+++ utils/dateTimeUtil.ts
@@ -182,3 +182,16 @@ export const parseISODateToDisplayDate = (isoDate: string): string => {
 
   return `${day}/${month}/${year}`;
 };
+
+export const parseISODateToDisplayDateTime = (isoDate: string): string => {
+  const date = new Date(isoDate);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes} ${day}/${month}/${year}`;
+};
