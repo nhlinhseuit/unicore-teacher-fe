5e3ae1c43b7c2db2576f15105e40a36bbfe0a8f5
nhlinhizota <93470801+nhlinhseuit@users.noreply.github.com>
2025-02-11 09:04:43 +0700

    fix code color

--- components/shared/PostItem/Item/ExercisePostItem.tsx
+++ components/shared/PostItem/Item/ExercisePostItem.tsx
@@ -11,7 +11,32 @@ import RenderFile from "../../Annoucements/RenderFile";
 import StatusButton from "../../Button/StatusButton";
 import Divider from "../../Divider";
 import { usePathname, useRouter } from "next/navigation";
+
+import Prism from "prismjs";
 import parse from "html-react-parser";
+
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-aspnet";
+import "prismjs/components/prism-sass";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-solidity";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-dart";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-r";
+import "prismjs/components/prism-kotlin";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-mongodb";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import { useEffect } from "react";
 
 interface Comment {
   id: string;
@@ -34,6 +59,10 @@ interface Props {
 const ExercisePostItem = (params: Props) => {
   const router = useRouter();
   const pathName = usePathname();
+
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
 
   return (
     <div className="card-wrapper rounded-[10px]">
--- components/shared/PostItem/Item/PostItem.tsx
+++ components/shared/PostItem/Item/PostItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { getAvatarName } from "@/lib/utils";
 import MyAvatar from "@/components/courses/MyAvatar";
@@ -7,8 +7,32 @@ import RenderFile from "../../Annoucements/RenderFile";
 import Divider from "../../Divider";
 import OtherComment from "@/components/courses/OtherComment";
 import MyComment from "@/components/courses/MyComment";
-import parse from "html-react-parser";
 import { usePathname, useRouter } from "next/navigation";
+
+import Prism from "prismjs";
+import parse from "html-react-parser";
+
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-csharp";
+import "prismjs/components/prism-aspnet";
+import "prismjs/components/prism-sass";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-solidity";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-dart";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-r";
+import "prismjs/components/prism-kotlin";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-mongodb";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 interface Comment {
   id: string;
@@ -30,6 +54,10 @@ const PostItem = (params: Props) => {
   const router = useRouter();
   const pathName = usePathname();
 
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+  
   return (
     <div className="card-wrapper rounded-[10px]">
       <div className="relative flex-col w-full p-6">
